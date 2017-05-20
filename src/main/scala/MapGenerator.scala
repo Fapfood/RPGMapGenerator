f57f89ef6221d431cb1cea96e65df7956daecdd6
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-05-20 16:33:07 +0200

    MapGenerator has dummu generateMap Method

--- src/main/scala/MapGenerator.scala
+++ src/main/scala/MapGenerator.scala
@@ -1,3 +1,6 @@
-class MapGenerator(val MapParames : MapParameters) {
-
+class MapGenerator(val MapParams : MapParameters) {
+  def generateMap(): Array[Array[MapField]] = {
+    var map = Array.ofDim[MapField](MapParams.width, MapParams.height)
+    map
+  }
 }
