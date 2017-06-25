e95a55d475e7d5912e75d90b5bc553d1429e8cbe
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-06-26 00:11:45 +0200

    Randomization works

--- src/main/scala/GUI/Controller.scala
+++ src/main/scala/GUI/Controller.scala
@@ -1,9 +1,23 @@
 package GUI
-object Controller {
 
+import scala.swing.TextField
+
+object Controller {
+  val r = scala.util.Random
+  val WidthRange : Range = Range(300,600)
+  val HeightRange : Range = Range(300, 600)
+  val TreesRange : Range = Range(0,25)
+  val HousesRange : Range = Range(0,10)
+  val ExitsRange : Range = Range(0,5)
+  val TextRangeMap = Map(MainWindow.ExitsTextField -> ExitsRange,
+    MainWindow.HeightTextField -> HeightRange,
+    MainWindow.HousesTextField -> HousesRange,
+    MainWindow.TreesTextField -> TreesRange,
+    MainWindow.WidthTextField -> WidthRange)
   def randomize(): Unit = {
     println("Randomizing")
-    MainWindow.HousesTextField.text = "321"
+  for((k,v) <- TextRangeMap) k.text = (v.min + (r.nextInt(v.max - v.min) + 1)).toString
+
   }
 
   def generate(): Unit = {
