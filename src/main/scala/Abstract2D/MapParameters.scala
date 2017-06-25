6f5b2287bc97bb9343cacb7ecb47d77bc6b7fd4b
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-06-26 00:17:32 +0200

    Added additional parameter

--- src/main/scala/Abstract2D/MapParameters.scala
+++ src/main/scala/Abstract2D/MapParameters.scala
@@ -1,5 +1,5 @@
 package Abstract2D
 
-class MapParameters private(val width: Int = 0, val height: Int = 0, val numberOfTrees: Int = 0, val numberOfHouses: Int = 0) {
+class MapParameters private(val width: Int = 0, val height: Int = 0, val numberOfTrees: Int = 0, val numberOfHouses: Int = 0, val numberOfExits: Int = 0) {
 
 }
