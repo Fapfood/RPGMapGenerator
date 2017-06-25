b518fb05c9395d76bb3f49747670912525271622
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-06-26 00:12:44 +0200

    Map parameters won't be a singleton after all.

--- src/main/scala/Abstract2D/MapParameters.scala
+++ src/main/scala/Abstract2D/MapParameters.scala
@@ -1,10 +1,5 @@
 package Abstract2D
 
-class MapParameters private(var width: Int = 0, var height: Int = 0, var numberOfTrees: Int = 0, var numberOfHouses: Int = 0) {
+class MapParameters private(val width: Int = 0, val height: Int = 0, val numberOfTrees: Int = 0, val numberOfHouses: Int = 0) {
 
-}
-
-object MapParameters{
-  private val _instance = new MapParameters()
-  def instance() = _instance
 }
