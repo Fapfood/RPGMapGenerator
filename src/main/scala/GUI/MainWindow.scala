253ffc4d31673e3ecf4354618c4bbfe03977b8d1
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-06-25 17:13:30 +0200

    Added simple text fields.

--- src/main/scala/GUI/MainWindow.scala
+++ src/main/scala/GUI/MainWindow.scala
@@ -3,11 +3,13 @@ import GUI.Controller
 import scala.swing._
 
 object MainWindow extends SimpleSwingApplication {
+  val WidthTextField: TextField = new TextField(columns = 10)
+  val HeightTextField: TextField = new TextField(columns = 10)
+  val TreesTextField: TextField = new TextField(columns = 10)
+  val HousesTextField: TextField = new TextField(columns = 10)
+  val ExitsTextField: TextField = new TextField(columns = 10)
   def top = new MainFrame {
     title = "First Swing App"
-    contents = new Button {
-      text = "Click me"
-    }
     menuBar = new MenuBar {
       contents += new Menu("Menu"){
         contents += new MenuItem(Action("Losuj dane"){
@@ -21,8 +23,41 @@ object MainWindow extends SimpleSwingApplication {
         })
       }
     }
-    size = new Dimension(500,500)
+
+    import BorderPanel.Position._
+    contents = getTextFieldsPanel
+   // size = new Dimension(500,500)
     centerOnScreen()
+  }
+
+  private[this] def getTextFieldsPanel: BoxPanel = {
+    new BoxPanel(Orientation.Vertical){
+      contents += new FlowPanel(){
+        contents += new Label("Map Width: ")
+        contents += WidthTextField
+      }
+
+      contents += new FlowPanel(){
+        contents += new Label("Map Height: ")
+        contents += HeightTextField
+      }
+
+      contents += new FlowPanel(){
+        contents += new Label("Trees Number: ")
+        contents += TreesTextField
+      }
+
+      contents += new FlowPanel(){
+        contents += new Label("Houses number: ")
+        contents += HousesTextField
+      }
+
+      contents += new FlowPanel(){
+        contents += new Label("Exits number: ")
+        contents += ExitsTextField
+      }
+
+    }
   }
 
 }
