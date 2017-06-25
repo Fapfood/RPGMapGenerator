9d0e561fc4d0b93618945e90a30da48251f9b21f
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-06-25 16:47:17 +0200

    Added menu bar to mainwindow, and controller with mock methods for communication.

---
+++ src/main/scala/GUI/Controller.scala
@@ -0,0 +1,15 @@
+package GUI
+
+object Controller {
+  def randomize: Unit = {
+    println("Randomizing")
+  }
+
+  def generate: Unit = {
+    println("Generating image")
+  }
+
+  def save: Unit = {
+    println("Saving file")
+  }
+}
--- src/main/scala/GUI/MainWindow.scala
+++ src/main/scala/GUI/MainWindow.scala
@@ -1,3 +1,5 @@
+import GUI.Controller
+
 import scala.swing._
 
 object MainWindow extends SimpleSwingApplication {
@@ -6,5 +8,21 @@ object MainWindow extends SimpleSwingApplication {
     contents = new Button {
       text = "Click me"
     }
+    menuBar = new MenuBar {
+      contents += new Menu("Menu"){
+        contents += new MenuItem(Action("Losuj dane"){
+          Controller.randomize
+        })
+        contents += new MenuItem(Action("Generuj"){
+          Controller.generate
+        })
+        contents += new MenuItem(Action("Zapisz"){
+          Controller.save
+        })
+      }
+    }
+    size = new Dimension(500,500)
+    centerOnScreen()
   }
+
 }
