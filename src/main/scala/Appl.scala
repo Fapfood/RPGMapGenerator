2552ef265713685eca1d41e0e631aace9d1645be
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-06-25 13:13:45 +0200

    Map Parameters are now a singleton.

--- src/main/scala/Appl.scala
+++ src/main/scala/Appl.scala
@@ -1,5 +1,9 @@
+import Abstract2D.MapParameters
+
 object Appl {
   def main(agrs: Array[String]): Unit = {
     println("Hello, world!")
+    MapParameters.instance.width = 3;
+    println(MapParameters.instance.width);
   }
 }
