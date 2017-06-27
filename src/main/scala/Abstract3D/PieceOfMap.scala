fa12b67ab7a11d71556afbb714b535259b46bbcc
Fapfood <fapfood@gmail.com>
2017-06-27 23:54:53 +0200

    add Grass, Ground, Fence

--- src/main/scala/Abstract3D/PieceOfMap.scala
+++ src/main/scala/Abstract3D/PieceOfMap.scala
@@ -1,6 +1,6 @@
 package Abstract3D
 
-import Abstract2DAncillary.Point
+import Ancillary.Point
 
 sealed abstract class PieceOfMap(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) {}
 
@@ -15,3 +15,9 @@ case class PieceOfRoof(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) 
 case class PieceOfDoor(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
 
 case class PieceOfSign(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
+
+case class PieceOfGrass(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
+
+case class PieceOfGround(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
+
+case class PieceOfFence(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
