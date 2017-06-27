dee0ec37b02648a29442b733ff176cce5f10a19a
Fapfood <fapfood@gmail.com>
2017-06-27 18:34:46 +0200

    add TileGenerator

---
+++ src/main/scala/Randomization/TileGenerator.scala
@@ -0,0 +1,33 @@
+package Randomization
+
+import Abstract2DAncillary.Point
+import Abstract3D.{PieceOfMap, PieceOfPath, PieceOfSign, PieceOfTree}
+import OutsideTiles.DirectTiles.{BroadTree, NarrowTree, Signpost}
+import Randomization.OutsideTiles.FoldingComplexTiles.Road
+import com.sksamuel.scrimage.Image
+
+class TileGenerator(piecesOfMap: List[PieceOfMap]) {
+
+  def generateMap(): List[(Point, Int, Image)] = {
+    val buff = collection.mutable.ListBuffer.empty[(Point, Int, Image)]
+
+    for (pieceOfMap <- piecesOfMap)
+      pieceOfMap match {
+        case tree: PieceOfTree =>
+          if (tree.mapNxM.flatten.length == 2)
+            buff += Tuple3(tree.point, tree.layer, NarrowTree.getTileFor(tree.mapNxM))
+          if (tree.mapNxM.flatten.length == 4)
+            buff += Tuple3(tree.point, tree.layer, BroadTree.getTileFor(tree.mapNxM))
+
+        case path: PieceOfPath =>
+          buff += Tuple3(path.point, path.layer, Road.getTileFor(path.mapNxM))
+
+        case sign: PieceOfSign =>
+          buff += Tuple3(sign.point, sign.layer, Signpost.getTileFor(sign.mapNxM))
+
+        case _ =>
+      }
+
+    buff.toList
+  }
+}
