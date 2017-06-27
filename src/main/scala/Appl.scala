4bc5b904ca0b4800adb922971a9152e58f24d43a
Fapfood <fapfood@gmail.com>
2017-06-28 00:52:06 +0200

    remove Appl

--- src/main/scala/Appl.scala
+++ src/main/scala/Appl.scala
@@ -1,5 +1,33 @@
+import java.io._
+
+import Abstract2D.Containers.StreetNetwork
+import Abstract2D.Map
+import Abstract2D.Objects.{Signpost, Tree}
+import Abstract3D.Abstract3DMapGenerator
+import Ancillary.Point
+import Randomization.TileGenerator
+import Generator.MapGenerator
+
 object Appl {
   def main(agrs: Array[String]) {
-    println("Hello world")
+
+    val map = new Map(5, 5)
+    map.addElement(new Signpost(Point(3, 4), Point(4, 4)))
+    map.addElement(new Signpost(Point(-1, 0), Point(0, 0)))
+    map.addElement(new Tree(List(Point(1, 1))))
+    map.addElement(new Tree(List(Point(1, 2))))
+    map.addElement(new Tree(List(Point(1, 3))))
+    map.addElement(new Tree(List(Point(2, 1))))
+    map.addElement(new Tree(List(Point(3, 3))))
+    map.addElement(new StreetNetwork(map))
+    val piecesOfMap = new Abstract3DMapGenerator(map).generateMap()
+    val piecesOfTile = new TileGenerator(piecesOfMap).generateMap()
+
+    val gen = new MapGenerator(5, 5)
+    val out = new File("C:\\Users\\FAPFOOD\\Desktop\\Test.png")
+    for (el <- piecesOfTile)
+      gen.addTileOn(el._3, el._1)
+    gen.image.output(out)
+
   }
 }
