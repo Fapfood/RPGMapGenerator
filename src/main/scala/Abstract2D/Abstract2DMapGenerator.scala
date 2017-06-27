3a7f25dd4d659300222b33dda10158d1a122973c
Fapfood <fapfood@gmail.com>
2017-06-27 23:47:21 +0200

    change dir

---
+++ src/main/scala/Abstract2D/Abstract2DMapGenerator.scala
@@ -0,0 +1,5 @@
+package Abstract2D
+
+import GUI.MapParameters
+
+class Abstract2DMapGenerator(val MapParams: MapParameters)
--- src/main/scala/Abstract2DAncillary/Abstract2DMapGenerator.scala
+++
@@ -1,8 +0,0 @@
-package Abstract2DAncillary
-
-import GUI.MapParameters
-import Abstract2DObjects._
-
-class Abstract2DMapGenerator(val MapParams: MapParameters) {
-
-}
