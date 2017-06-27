2ba6c60db3e5ce3a7a9f60756bc0c857e77a923d
Fapfood <fapfood@gmail.com>
2017-06-27 23:04:37 +0200

    add Door, use Trait

--- src/main/scala/Abstract2DObjects/Building.scala
+++ src/main/scala/Abstract2DObjects/Building.scala
@@ -1,15 +1,25 @@
 package Abstract2DObjects
 
-import Abstract2DAncillary.{BlockObject, Point}
+import Abstract2DAncillary.{BlockObject, Perimeter, Point, RectangularObject}
 
 import scala.util.Random
 
 class Building(leftTopCorner: Point, rightDownCorner: Point, door: Point, wallHeight: Int)
   extends MapObject with BlockObject {
-  override val pointsList: List[Point] = generateFloor()
-  override val heightOfStorey: Int = wallHeight
-  override val numberOfStoreys: Int = 1
+  override val pointsList: List[Point] = new RectangularObject {}.buildBase(leftTopCorner, rightDownCorner)
+  override protected val heightOfStorey: Int = wallHeight
+  override protected val numberOfStoreys: Int = 1
   override val hardness: Int = 1
+  override val entryPoint: Option[Point] = Option(chooseDoor())
+
+  private def chooseDoor(): Point = {
+    val points = new Perimeter {}.getPerimeter(pointsList)
+      .filter(p => if (p.x < leftTopCorner.x + 1
+        || p.x > rightDownCorner.x - 1
+        || p.y < leftTopCorner.y + 1
+        || p.y > rightDownCorner.y - 1) false else true)
+    points(Random.nextInt(points.length))
+  }
 
   //  object CasualShape extends Enumeration {
   //    type Shape = Value
@@ -24,12 +34,4 @@ class Building(leftTopCorner: Point, rightDownCorner: Point, door: Point, wallHe
   //
   //    def randomShape(): Shape = EpicShape(Random.nextInt(EpicShape.maxId))
   //  }
-
-  private def generateFloor(): List[Point] = {
-    val buff = collection.mutable.ListBuffer.empty[Point]
-    for (i <- leftTopCorner.x to rightDownCorner.x)
-      for (j <- leftTopCorner.y to rightDownCorner.y)
-        buff += Point(i, j)
-    buff.toList
-  }
 }
