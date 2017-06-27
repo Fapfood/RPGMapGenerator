b0b4e29d66cf0940b48c2c15af3c5cd9b8bc4378
Fapfood <fapfood@gmail.com>
2017-06-27 22:54:50 +0200

    add RectangularObject

---
+++ src/main/scala/Abstract2DAncillary/RectangularObject.scala
@@ -0,0 +1,13 @@
+package Abstract2DAncillary
+
+trait RectangularObject {
+  def buildBase(leftTop: Point, rightBottom: Point): List[Point] = {
+    val buff = collection.mutable.ListBuffer.empty[Point]
+
+    for (x <- leftTop.x to rightBottom.x)
+      for (y <- leftTop.y to rightBottom.y)
+        buff += Point(x, y)
+
+    buff.toList
+  }
+}
