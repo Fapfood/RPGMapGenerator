0a1245153ce910772391b524b65c96f1f171c969
Fapfood <fapfood@gmail.com>
2017-06-27 23:45:17 +0200

    remove protected

--- src/main/scala/Abstract2D/Objects/Tree.scala
+++ src/main/scala/Abstract2D/Objects/Tree.scala
@@ -1,6 +1,7 @@
-package Abstract2DObjects
+package Abstract2D.Objects
 
-import Abstract2DAncillary.Point
+import Abstract2D.Ancillary.MapObject
+import Ancillary.Point
 
 class Tree(listOfRootFields: List[Point]) extends MapObject {
   override val pointsList: List[Point] = listOfRootFields
