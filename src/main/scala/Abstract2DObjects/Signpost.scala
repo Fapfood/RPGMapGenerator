a319e6351271020c3b2416c808fd1c626987c42e
Fapfood <fapfood@gmail.com>
2017-06-27 23:00:26 +0200

    remove protected

--- src/main/scala/Abstract2DObjects/Signpost.scala
+++ src/main/scala/Abstract2DObjects/Signpost.scala
@@ -3,7 +3,7 @@ package Abstract2DObjects
 import Abstract2DAncillary.Point
 
 class Signpost(signpoint: Point, entrypoint: Point) extends MapObject {
-  override protected val pointsList: List[Point] = List(signpoint)
-  override protected val hardness: Int = 1
-  override protected val entryPoint: Option[Point] = Option(entrypoint)
+  override val pointsList: List[Point] = List(signpoint)
+  override val hardness: Int = 1
+  override val entryPoint: Option[Point] = Option(entrypoint)
 }
