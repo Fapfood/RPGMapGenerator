16648d9ee974cb4eb330c7d45e1a4c9f22dd7c10
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-06-28 22:05:54 +0200

    When clicking generate a confirmation box happens, which display an image (no actions for yes/no yet)

--- src/main/scala/GUI/Controller.scala
+++ src/main/scala/GUI/Controller.scala
@@ -1,6 +1,15 @@
 package GUI
 
-import scala.swing.TextField
+import com.sksamuel.scrimage.Image
+import Abstract2D.Containers.{AllGrass, AllGround, AllPaths}
+import Abstract2D.Map
+import Abstract2D.Objects._
+import Abstract3D.Abstract3DMapGenerator
+import Ancillary.Point
+import Randomization.TileGenerator
+import Generator.MapGenerator
+import java.io._
+import javax.swing.ImageIcon
 
 object Controller {
   val r = scala.util.Random
@@ -24,6 +33,12 @@ object Controller {
     println("Generating image")
     val params = new MapParameters(width, height, treesNumber, housesNumber, exitsNumber)
     //todo: call method which will return image, then print the image.
+    val image : Image = createDebugMap()
+    val out : File = new File("tmp.png")
+    image.output(out)
+
+    val img = new ImageIcon("tmp.png")
+    scala.swing.Dialog.showConfirmation(message = null, icon = img, title = "Do you want to save this image?")
   }
 
   def save(): Unit = {
@@ -44,5 +59,26 @@ object Controller {
   }
   def exitsNumber : Int = {
     MainWindow.ExitsTextField.text.toInt
+  }
+
+  private def createDebugMap(): Image = {
+    val map = new Map(25, 25)
+    map.addElement(new AllGrass(new Grass(Point(0, 0), Point(24, 24)), new AllGround(List.empty[Ground])))
+    map.addElement(new Signpost(Point(3, 4), Point(4, 4)))
+    map.addElement(new Signpost(Point(-1, 0), Point(0, 0)))
+    map.addElement(new Tree(List(Point(1, 1))))
+    map.addElement(new Tree(List(Point(1, 2))))
+    map.addElement(new Tree(List(Point(1, 3))))
+    map.addElement(new Tree(List(Point(2, 1))))
+    map.addElement(new Tree(List(Point(3, 3))))
+    map.addElement(new Building(Point(6, 6), Point(9, 8), 2))
+    map.addElement(new AllPaths(map))
+    val piecesOfMap = new Abstract3DMapGenerator(map).generateMap()
+    val piecesOfTile = new TileGenerator(piecesOfMap).generateMap()
+
+    val gen = new MapGenerator(25, 25)
+    for (el <- piecesOfTile)
+      gen.addTileOn(el._3, el._1)
+    gen.image
   }
 }
