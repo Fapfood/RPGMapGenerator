9cc7039a402c4b8344b4e91ae6b625659641e0f5
Fapfood <fapfood@gmail.com>
2017-06-29 19:06:55 +0200

    add addTitles

--- src/main/scala/Generator/MapGenerator.scala
+++ src/main/scala/Generator/MapGenerator.scala
@@ -7,9 +7,21 @@ import com.sksamuel.scrimage.{Image, Position}
 
 class MapGenerator(x: Int, y: Int) {
   private val TILE_SIDE = 32
-  var image: Image = makeImage(x, y)
+  private var image: Image = makeImage(x, y)
 
-  def addTileOn(tile: Image, position: Point): Unit = {
+  def addTiles(tuples: List[(Point, Int, Image)]): Image = {
+    val buff = tuples.to[collection.mutable.ListBuffer]
+    while (buff.nonEmpty) {
+      val priority = buff.map(v => v._2).min
+      val withThisPriority = buff.filter(v => v._2 == priority)
+      buff --= withThisPriority
+      for (b <- withThisPriority)
+        addTileOn(b._3, b._1)
+    }
+    image
+  }
+
+  private def addTileOn(tile: Image, position: Point): Unit = {
     image = image.overlay(tile, TILE_SIDE * position.x, TILE_SIDE * position.y)
   }
 
