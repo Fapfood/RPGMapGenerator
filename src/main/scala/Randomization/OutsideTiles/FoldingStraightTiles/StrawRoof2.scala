6d1d84269903422dc19aea9487dedb7e32cc2a51
Fapfood <fapfood@gmail.com>
2017-06-29 21:13:03 +0200

    new Roof and Wall

---
+++ src/main/scala/Randomization/OutsideTiles/FoldingStraightTiles/StrawRoof2.scala
@@ -0,0 +1,5 @@
+package Randomization.OutsideTiles.FoldingStraightTiles
+
+import Randomization.FoldingStraightTile
+
+object StrawRoof2 extends FoldingStraightTile("outside\\A13.png", 12, 0)
---
+++ src/main/scala/Randomization/OutsideTiles/FoldingStraightTiles/WoodenStumpsWall.scala
@@ -0,0 +1,5 @@
+package Randomization.OutsideTiles.FoldingStraightTiles
+
+import Randomization.FoldingStraightTile
+
+object WoodenStumpsWall extends FoldingStraightTile("outside\\A3.png", 6, 6)
