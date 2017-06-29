b65e8b83833d2a68763782b3bff4f788a54cc45a
Fapfood <fapfood@gmail.com>
2017-06-29 17:17:29 +0200

    change Building

--- src/main/scala/Abstract2D/Objects/Building.scala
+++ src/main/scala/Abstract2D/Objects/Building.scala
@@ -5,21 +5,22 @@ import Ancillary.{Perimeter, Point}
 
 import scala.util.Random
 
-class Building(val leftTopCorner: Point, val rightBottomCorner: Point, wallHeight: Int)
+class Building(val topLeft: Point, val bottomRight: Point, workArea: List[Point], wallHeight: Int)
   extends MapObject with BlockObject {
-  override val pointsList: List[Point] = new RectangularObject {}.buildBase(leftTopCorner, rightBottomCorner)
+  override val pointsList: List[Point] = new RectangularObject {}.buildBase(topLeft, bottomRight)
   override val heightOfStorey: Int = wallHeight
   override val numberOfStoreys: Int = 1
   override val hardness: Int = 1
   override val entryPoint: Option[Point] = Option(chooseDoor())
 
   private def chooseDoor(): Point = {
-    val points = new Perimeter {}.getPerimeter(pointsList)
+    val points = Perimeter.getPerimeter(pointsList)
       .filter(p => if (
-        (p.x >= leftTopCorner.x + 1
-          && p.x <= rightBottomCorner.x - 1)
-          || (p.y >= leftTopCorner.y + 1
-          && p.y <= rightBottomCorner.y - 1)
+        ((p.x >= topLeft.x + 1
+          && p.x <= bottomRight.x - 1)
+          || (p.y >= topLeft.y + 1
+          && p.y <= bottomRight.y - 1))
+          && workArea.contains(p)
       ) true else false)
     points(Random.nextInt(points.length))
   }
