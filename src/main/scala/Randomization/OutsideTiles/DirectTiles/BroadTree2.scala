c9e88f7204b70b1bfbd10583dfc86d6c9dcabbeb
Fapfood <fapfood@gmail.com>
2017-06-29 20:57:24 +0200

    Trees

---
+++ src/main/scala/Randomization/OutsideTiles/DirectTiles/BroadTree2.scala
@@ -0,0 +1,5 @@
+package Randomization.OutsideTiles.DirectTiles
+
+import Randomization.DirectTile
+
+object BroadTree2 extends DirectTile("outside\\B2.png", 6, 7, 4, 5)
---
+++ src/main/scala/Randomization/OutsideTiles/DirectTiles/NarrowDeadTree.scala
@@ -0,0 +1,5 @@
+package Randomization.OutsideTiles.DirectTiles
+
+import Randomization.DirectTile
+
+object NarrowDeadTree extends DirectTile("outside\\B.png", 8, 8, 5, 6)
