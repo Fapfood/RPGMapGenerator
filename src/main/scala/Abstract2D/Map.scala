01eaf59aea4500bb3f626e151022c44bb751bea5
Fapfood <fapfood@gmail.com>
2017-06-29 17:20:58 +0200

    add methods

--- src/main/scala/Abstract2D/Map.scala
+++ src/main/scala/Abstract2D/Map.scala
@@ -1,14 +1,29 @@
 package Abstract2D
 
-import Abstract2D.Ancillary.MapObject
-import _root_.Ancillary.{Perimeter, Point}
+import Abstract2D.Ancillary.{MapObject, RectangularObject}
+import _root_.Ancillary.Point
 
-class Map(val x: Int, val y: Int) extends Perimeter {
+class Map(val x: Int, val y: Int) {
   private val elems = collection.mutable.ListBuffer.empty[MapObject]
+  val pointsList: List[Point] = new RectangularObject {}.buildBase(Point(), Point(x - 1, y - 1))
 
   def addElement(elem: MapObject): Unit = elems += elem
 
   def getElements: List[MapObject] = elems.toList
+
+  def getTopLeftCorner: Point = Point(0, 0)
+
+  def getBottomRightCorner: Point = Point(x, y)
+
+  def belongsToMap(point: Point): Boolean = pointsList.contains(point)
+
+  def getObstacles: List[Point] = {
+    val obstacles = collection.mutable.ListBuffer.empty[Point]
+    for (p <- pointsList)
+      if (isHardOccupied(p))
+        obstacles += p
+    obstacles.toList
+  }
 
   def isHardOccupied(point: Point): Boolean = {
     for (o <- elems)
@@ -16,28 +31,5 @@ class Map(val x: Int, val y: Int) extends Perimeter {
         return true
 
     false
-  }
-
-  def getGroupOfHardPointsConnectedWith(point: Point): List[Point] = {
-    if (!isHardOccupied(point))
-      throw new Exception("Wrong point list in getGroupOfHardPointsConnectedWith")
-
-    @scala.annotation.tailrec
-    def group(points: List[Point]): List[Point] = {
-
-      val buff = points.to[collection.mutable.ListBuffer]
-
-      for (p <- getPerimeter(points)) {
-        if (isHardOccupied(p))
-          buff += p
-      }
-
-      if (buff.length == points.length)
-        return points
-
-      group(buff.toList)
-    }
-
-    group(List(point))
   }
 }
