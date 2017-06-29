afd90860c7b660d1ee97f8a098061c6c876d5246
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-06-29 11:56:59 +0200

    Test if points are connected in BetweenPointsObject

--- src/test/scala/Abstract2D/Ancillary/BetweenPointsObjectTest.scala
+++ src/test/scala/Abstract2D/Ancillary/BetweenPointsObjectTest.scala
@@ -14,4 +14,15 @@ class BetweenPointsObjectTest extends FunSuite {
     assert(a.head == p1)
     assert(a.last == p2)
   }
+
+  test("Points are connected"){
+    val p1 = Point(3,4)
+    val p2 = Point(12,14)
+    val a : List[Point] = new BetweenPointsObject {}.planIdealPath(p1,p2)
+    a.sliding(2).foreach{twoPoints =>
+      val (pp1, pp2) = (twoPoints.head, twoPoints.last)
+      assert(math.abs(pp1.x - pp2.x) <= 1)
+      assert(math.abs(pp1.y - pp2.y) <= 1)
+    }
+  }
 }
