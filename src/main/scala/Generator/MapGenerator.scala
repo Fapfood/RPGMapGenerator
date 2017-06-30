41b21b5b743a69372f939d1bd9cac96859294419
Fapfood <fapfood@gmail.com>
2017-06-30 18:07:36 +0200

    remove ()

--- src/main/scala/Generator/MapGenerator.scala
+++ src/main/scala/Generator/MapGenerator.scala
@@ -10,8 +10,8 @@ import com.sksamuel.scrimage.Image
 object MapGenerator {
   def createMapWithParameters(mapParams: MapParameters): Image = {
     val map = new Abstract2DMapGenerator(mapParams).generateMap
-    val piecesOfMap = new Abstract3DMapGenerator(map).generateMap()
-    val piecesOfTile = new TileGenerator(piecesOfMap).generateMap()
+    val piecesOfMap = new Abstract3DMapGenerator(map).generateMap
+    val piecesOfTile = new TileGenerator(piecesOfMap).generateMap
     val image = new ImageCreator(mapParams.width, mapParams.height).addTiles(piecesOfTile)
     image
   }
