0bb13b28cf6ef733120775f8323bbb98a113b580
Fapfood <fapfood@gmail.com>
2017-06-30 18:01:49 +0200

    pull subclasses

--- src/main/scala/Abstract3D/PieceOfMap.scala
+++ src/main/scala/Abstract3D/PieceOfMap.scala
@@ -2,22 +2,4 @@ package Abstract3D
 
 import Ancillary.Point
 
-sealed abstract class PieceOfMap(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) {}
-
-case class PieceOfTree(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
-
-case class PieceOfPath(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
-
-case class PieceOfWall(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
-
-case class PieceOfRoof(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
-
-case class PieceOfDoor(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
-
-case class PieceOfSign(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
-
-case class PieceOfGrass(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
-
-case class PieceOfGround(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
-
-case class PieceOfFence(point: Point, layer: Int, mapNxM: Array[Array[Boolean]]) extends PieceOfMap(point, layer, mapNxM)
+abstract class PieceOfMap(point: Point, layer: Int, mapNxM: Array[Array[Boolean]], randomize: Int)
