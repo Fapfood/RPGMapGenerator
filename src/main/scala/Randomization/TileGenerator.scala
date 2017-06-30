bf9ab4ee9b3aa5a2b55a6e6750704c4a2374e878
Fapfood <fapfood@gmail.com>
2017-06-30 18:05:19 +0200

    add randomization

--- src/main/scala/Randomization/TileGenerator.scala
+++ src/main/scala/Randomization/TileGenerator.scala
@@ -1,30 +1,43 @@
 package Randomization
 
+import Abstract3D.PiecesOfMap._
 import Abstract3D._
 import Ancillary.Point
-import OutsideTiles.DirectTiles._
-import OutsideTiles.FoldingComplexTiles._
-import OutsideTiles.FoldingStraightTiles._
+import Randomization.OutsideTiles.DirectTiles._
+import Randomization.OutsideTiles.FoldingComplexTiles._
+import Randomization.OutsideTiles.FoldingStraightTiles._
 import com.sksamuel.scrimage.Image
 
 class TileGenerator(piecesOfMap: List[PieceOfMap]) {
 
-  def generateMap(): List[(Point, Int, Image)] = {
+  def generateMap: List[(Point, Int, Image)] = {
     val buff = collection.mutable.ListBuffer.empty[(Point, Int, Image)]
 
     for (pieceOfMap <- piecesOfMap)
       pieceOfMap match {
         case tree: PieceOfTree =>
           if (tree.mapNxM.flatten.length == 2)
-            buff += Tuple3(tree.point, tree.layer, NarrowTree.getTileFor(tree.mapNxM))
+            tree.randomize match {
+              case 0 => buff += Tuple3(tree.point, tree.layer, NarrowDeadTree.getTileFor(tree.mapNxM))
+              case 1 => buff += Tuple3(tree.point, tree.layer, NarrowTree2.getTileFor(tree.mapNxM))
+              case _ => buff += Tuple3(tree.point, tree.layer, NarrowTree.getTileFor(tree.mapNxM))
+            }
           if (tree.mapNxM.flatten.length == 4)
-            buff += Tuple3(tree.point, tree.layer, BroadTree.getTileFor(tree.mapNxM))
+            tree.randomize match {
+              case 0 => buff += Tuple3(tree.point, tree.layer, BroadTree.getTileFor(tree.mapNxM))
+              case 1 => buff += Tuple3(tree.point, tree.layer, BroadConifer.getTileFor(tree.mapNxM))
+              case 2 => buff += Tuple3(tree.point, tree.layer, BroadTree2.getTileFor(tree.mapNxM))
+              case _ => buff += Tuple3(tree.point, tree.layer, BroadDeadTree.getTileFor(tree.mapNxM))
+            }
 
         case path: PieceOfPath =>
           buff += Tuple3(path.point, path.layer, Road.getTileFor(path.mapNxM))
 
         case sign: PieceOfSign =>
-          buff += Tuple3(sign.point, sign.layer, Signpost.getTileFor(sign.mapNxM))
+          sign.randomize match {
+            case 0 => buff += Tuple3(sign.point, sign.layer, Signpost.getTileFor(sign.mapNxM))
+            case _ => buff += Tuple3(sign.point, sign.layer, Signpost2.getTileFor(sign.mapNxM))
+          }
 
         case grass: PieceOfGrass =>
           buff += Tuple3(grass.point, grass.layer, Grass.getTileFor(grass.mapNxM))
@@ -33,10 +46,16 @@ class TileGenerator(piecesOfMap: List[PieceOfMap]) {
           buff += Tuple3(ground.point, ground.layer, Ground.getTileFor(ground.mapNxM))
 
         case wall: PieceOfWall =>
-          buff += Tuple3(wall.point, wall.layer, NarrowBoardsWall.getTileFor(wall.mapNxM))
+          wall.randomize match {
+            case 0 => buff += Tuple3(wall.point, wall.layer, NarrowBoardsWall.getTileFor(wall.mapNxM))
+            case _ => buff += Tuple3(wall.point, wall.layer, WoodenStumpsWall.getTileFor(wall.mapNxM))
+          }
 
         case roof: PieceOfRoof =>
-          buff += Tuple3(roof.point, roof.layer, StrawRoof.getTileFor(roof.mapNxM))
+          roof.randomize match {
+            case 0 => buff += Tuple3(roof.point, roof.layer, StrawRoof.getTileFor(roof.mapNxM))
+            case _ => buff += Tuple3(roof.point, roof.layer, StrawRoof2.getTileFor(roof.mapNxM))
+          }
 
         case door: PieceOfDoor =>
           buff += Tuple3(door.point, door.layer, Door.getTileFor(door.mapNxM))
