c90e1175892c1e30c4af973c9732f132d3fe5197
Fapfood <fapfood@gmail.com>
2017-07-01 01:47:48 +0200

    rename

--- src/main/scala/Generator/MapGenerator.scala
+++ src/main/scala/Generator/MapGenerator.scala
@@ -1,7 +1,7 @@
 package Generator
 
 import Abstract2D.Abstract2DMapGenerator
-import Abstract3D.Abstract3DMapGenerator
+import Abstract3D.PieceOfMapGenerator
 import GUI.MapParameters
 import Randomization.TileGenerator
 import com.sksamuel.scrimage.Image
@@ -10,7 +10,7 @@ import com.sksamuel.scrimage.Image
 object MapGenerator {
   def createMapWithParameters(mapParams: MapParameters): Image = {
     val map = new Abstract2DMapGenerator(mapParams).generateMap
-    val piecesOfMap = new Abstract3DMapGenerator(map).generateMap
+    val piecesOfMap = new PieceOfMapGenerator(map).generateMap
     val piecesOfTile = new TileGenerator(piecesOfMap).generateMap
     val image = new ImageCreator(mapParams.width, mapParams.height).addTiles(piecesOfTile)
     image
