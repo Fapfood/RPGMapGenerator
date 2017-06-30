7be771c531e145d54902cda861c4b7dbb4c521a6
Fapfood <fapfood@gmail.com>
2017-06-30 18:09:32 +0200

    add generator

--- src/main/scala/Abstract2D/Abstract2DMapGenerator.scala
+++ src/main/scala/Abstract2D/Abstract2DMapGenerator.scala
@@ -1,9 +1,11 @@
 package Abstract2D
 
-import Abstract2D.Containers.{AllGrass, AllGround, AllPaths}
+import Abstract2D.Base._
 import Abstract2D.Objects._
-import _root_.Ancillary.Point
 import GUI.MapParameters
+import _root_.Ancillary.{MapVisualizer, Point}
+
+import scala.collection.concurrent.TrieMap
 import scala.util.Random
 
 class Abstract2DMapGenerator(val mapParams: MapParameters) {
@@ -14,32 +16,57 @@ class Abstract2DMapGenerator(val mapParams: MapParameters) {
     map.addElement(
       new AllGrass(new Grass(map.getTopLeftCorner, map.getBottomRightCorner), new AllGround(List.empty[Ground])))
 
-    for (_ <- 1 to mapParams.numberOfHouses)
-      map.addElement(generateBuilding(map))
+    for (_ <- 1 to mapParams.numberOfHouses) {
+      val building = generateBuilding(map)
+      if (building.nonEmpty)
+        map.addElement(building.get)
+    }
 
-    for (i <- 1 to mapParams.numberOfTrees)
-      map.addElement(generateTree(map))
+    val board = new TrieMap[Point, String]()
+    for (el <- map.getElements.filter(!_.isInstanceOf[AllGrass]).flatten(_.pointsList))
+      board.putIfAbsent(el, "D")
 
-    //    map.addElement(new Signpost(Point(8, 0), Point(9, 0)))
-    //    map.addElement(new Signpost(Point(0, 1), Point(0, 0)))
+    for (_ <- 1 to mapParams.numberOfTrees) {
+      val tree = generateTree(map)
+      if (tree.nonEmpty)
+        map.addElement(tree.get)
+    }
 
-//    map.addElement(new AllPaths(map))
+    for (el <- map.getElements.filter(!_.isInstanceOf[AllGrass]).flatten(_.pointsList))
+      board.putIfAbsent(el, "T")
+
+    for (_ <- 1 to mapParams.numberOfExits) {
+      val sign = generateSign(map)
+      if (sign.nonEmpty)
+        map.addElement(sign.get)
+    }
+
+    for (el <- map.getElements.filter(!_.isInstanceOf[AllGrass]).flatten(_.pointsList))
+      board.putIfAbsent(el, "S")
+    println(new MapVisualizer().dump(board.toMap))
+
+
+    map.addElement(new AllPaths(map))
     map
   }
 
-  private def generateBuilding(map: Map): Building = {
+  private def generateBuilding(map: Map): Option[Building] = {
+    var tried = 0
     var building: Building = new Building(Point(), Point(), List(), 0)
     do {
       val width = getRandomInt(0.6)
-      val height = getRandomInt(0.5)
+      val height = getRandomInt(0.4)
       val x = Random.nextInt(mapParams.width - width)
       val y = Random.nextInt(mapParams.height - height - 2) + 2
       building = new Building(Point(x, y), Point(x + width - 1, y + height - 1), map.pointsList, 2)
-    } while (!isAreaFree(building.pointsList, map.getObstacles) || !belongAreaToArea(building.pointsList, map.pointsList))
-    building
+      tried += 1
+    } while (!isAreaFree(building.pointsList ++ building.entryPoint, map.getObstacles)
+      || !belongAreaToArea(building.pointsList ++ building.entryPoint, map.pointsList) || tried >= 20)
+    if (tried < 20) Option(building) else None
   }
 
-  private def generateTree(map: Map): Tree = {
+  private def generateTree(map: Map): Option[Tree] = {
+    var tried = 0
     var tree: Tree = new Tree(List())
     do {
       val width = Random.nextInt(2) + 1
@@ -47,8 +74,28 @@ class Abstract2DMapGenerator(val mapParams: MapParameters) {
       val y = Random.nextInt(mapParams.height - 1) + 1
       val roots = if (width == 1) List(Point(x, y)) else List(Point(x, y), Point(x + 1, y))
       tree = new Tree(roots)
-    } while (!isAreaFree(tree.pointsList, map.getObstacles) || !belongAreaToArea(tree.pointsList, map.pointsList))
-    tree
+      tried += 1
+    } while (!isAreaFree(tree.pointsList, map.getObstacles)
+      || !belongAreaToArea(tree.pointsList, map.pointsList) || tried >= 20)
+    if (tried < 20) Option(tree) else None
+  }
+
+  private def generateSign(map: Map): Option[Signpost] = {
+    var tried = 0
+    var sign: Signpost = new Signpost(Point(), Point())
+    do {
+      val x = Random.nextInt(mapParams.width)
+      val y = Random.nextInt(mapParams.height)
+      val signPoint = List(Point(x, 0), Point(x, mapParams.height - 1), Point(0, y), Point(mapParams.width - 1, y))
+        .apply(Random.nextInt(4))
+      val entryPoints = List(signPoint + Point(0, 1), signPoint - Point(0, 1), signPoint + Point(1, 0), signPoint - Point(1, 0))
+        .filter(map.pointsList.contains(_))
+      val entryPoint = entryPoints(Random.nextInt(entryPoints.length))
+      sign = new Signpost(signPoint, entryPoint)
+      tried += 1
+    } while (!isAreaFree(sign.pointsList ++ sign.entryPoint, map.getObstacles)
+      || !belongAreaToArea(sign.pointsList ++ sign.entryPoint, map.pointsList) || tried >= 20)
+    if (tried < 20) Option(sign) else None
   }
 
   private def getRandomInt(factor: Double): Int = {
@@ -57,7 +104,7 @@ class Abstract2DMapGenerator(val mapParams: MapParameters) {
     while (Random.nextDouble() <= factor)
       i += 1
 
-    if (i < 2) 2 else i
+    if (i < 3) 3 else if (i > Math.max(mapParams.width, mapParams.height) - 5) 8 else i
   }
 
   private def isAreaFree(area: List[Point], obstacles: List[Point]): Boolean =
