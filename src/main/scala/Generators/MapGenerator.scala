33ea7044f060dfec1a344e8993045d56c814a078
Fapfood <fapfood@gmail.com>
2017-07-04 19:19:42 +0200

    add BaseGenerator layer, add printlns

--- src/main/scala/Generators/MapGenerator.scala
+++ src/main/scala/Generators/MapGenerator.scala
@@ -1,17 +1,20 @@
-package Generator
+package Generators
 
-import Abstract2D.Abstract2DMapGenerator
-import Abstract3D.PieceOfMapGenerator
 import GUI.MapParameters
-import Randomization.TileGenerator
 import com.sksamuel.scrimage.Image
 
 
 object MapGenerator {
   def createMapWithParameters(mapParams: MapParameters): Image = {
+    println(">> generating 2D map")
     val map = new Abstract2DMapGenerator(mapParams).generateMap
-    val piecesOfMap = new PieceOfMapGenerator(map).generateMap
+    println(">> generating base for the map")
+    val baseMap = new BaseGenerator(map).generateMap
+    println(">> generating arrays of elements")
+    val piecesOfMap = new PieceOfMapGenerator(baseMap).generateMap
+    println(">> generating tiles")
     val piecesOfTile = new TileGenerator(piecesOfMap).generateMap
+    println(">> generating image")
     val image = new ImageCreator(mapParams.width, mapParams.height).addTiles(piecesOfTile)
     image
   }
