1a8c78109bc3cc34567c8e3c819ca8b2d6bdd273
Fapfood <fapfood@gmail.com>
2017-07-07 23:32:54 +0200

    cosmetic changes with names, dirs, imports and whitespaces

--- src/main/scala/Randomization/DTO_Tile.scala
+++ src/main/scala/Randomization/DTO_Tile.scala
@@ -3,4 +3,4 @@ package Randomization
 import Ancillary.Point
 import com.sksamuel.scrimage.Image
 
-class Tile(val point: Point, val tile: Image, val layer: Int)
+class DTO_Tile(val point: Point, val tile: Image, val layer: Int)
