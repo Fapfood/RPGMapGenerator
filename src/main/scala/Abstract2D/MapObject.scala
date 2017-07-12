b6f4dd35daefc3afc8f30557496eafe6c817c83a
Fapfood <fapfood@gmail.com>
2017-07-12 21:04:03 +0200

    change id to String

--- src/main/scala/Abstract2D/MapObject.scala
+++ src/main/scala/Abstract2D/MapObject.scala
@@ -5,6 +5,7 @@ import _root_.Ancillary.Point
 trait MapObject {
   val pointsList: List[Point]
   val hardness: Int
+  val id: String = this.toString
   val entryPoint: Option[Point] = None
 
   def getLevelOfOccupancy(point: Point): Int = {
